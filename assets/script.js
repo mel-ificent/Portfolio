fabb2cbf51d9e829636cfc0084709afc18a7abb4
Narcisah <91091964+mel-ificent@users.noreply.github.com>
2022-02-26 21:02:17 -0300

    updated projects

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,11 @@
 let imgHighlightEl = document.getElementById("img-highlight");
 let imgProjOneEl = document.getElementById("img-proj-one");
+let imgProjTwoEl = document.getElementById("img-proj-two");
 
 
 //Functions to update sample work images to a screenshot of work whenever user hovers over link
 function projOneImg() {
-    imgProjOneEl.setAttribute("src","./Images/weatherapp.jpg")
+    imgProjOneEl.setAttribute("src","./Images/newpost.jpg")
 
   }
   
@@ -13,10 +14,19 @@ function projOneImgReset() {
   }
 
 function highlightImg() {
-    imgHighlightEl.setAttribute("src","./Images/youluckydog.jpg")
+    imgHighlightEl.setAttribute("src","./Images/mevsu.jpg")
 
   }
   
 function highlightImgReset() {
     imgHighlightEl.setAttribute("src","./Images/tablet.jpg")
+  }
+
+  function projTwoImg() {
+    imgProjTwoEl.setAttribute("src","./Images/youluckydog.jpg")
+
+  }
+  
+function projTwoImgReset() {
+    imgProjTwoEl.setAttribute("src","./Images/pencils.jpg")
   }
